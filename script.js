5f6523177f853f949cf0ed9e55b62b535ef4d71e
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2023-05-12 09:13:26 +0200

    responsive mobile OK

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const openMenu = document.querySelector('.open-menu')
 const closeMenu = document.querySelector('.close-menu')
+const navBar= document.querySelector('.navbar')
+const filter= document.querySelector('.filter')
+
+openMenu.addEventListener('click', ()=>{
+    navBar.classList.toggle('active')
+    openMenu.classList.remove('active')
+    closeMenu.classList.add('active')
+    filter.classList.toggle('active')
+})
+
+
+closeMenu.addEventListener('click', ()=>{
+    navBar.classList.toggle('active')
+    openMenu.classList.add('active')
+    closeMenu.classList.remove('active')
+    filter.classList.toggle('active')
+})
